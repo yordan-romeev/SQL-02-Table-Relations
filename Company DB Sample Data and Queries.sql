5ec28fc601284b7660e7460c0ef45d778bbafdf5
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-13 16:23:58 +0200

    Add DepartmentID info to Sample Data

--- Company DB Sample Data and Queries.sql
+++ Company DB Sample Data and Queries.sql
@@ -2,61 +2,67 @@ USE Company;
 
 -- Insert rows into table 'Employees'
 INSERT INTO [Employees]
-( -- Columns to insert data into
+    ( -- Columns to insert data into
     [Name], [JobTitle], [Email]
-)
+    )
 VALUES
-(  'Peter Johnson', 'CEO', 'Peter.Johnson@company.mail' ),
-(  'Jack Richard', 'CTO', 'Jack.Richard@company.mail' ),
-(  'Maya Tomas', 'CFO', 'Maya.Tomas@company.mail' ),
-(  'Tomas Lindt', 'Lead Software Engineer', 'Tomas.Lindt@company.mail' ),
-(  'George Russel', 'Software Engineer', 'George.Russel@company.mail' ),
-(  'Anna Rocksmith', 'Junior Software Engineer', 'Anna.Rocksmith@company.mail' ),
-(  'Lily Jones', 'Lead Accountant', 'Lily.Jones@company.mail' ),
-(  'Lionel Ross', 'Accountant', 'Lionel.Ross@company.mail' ),
-(  'Star Light', 'Sales Lead', 'Star.Light@company.mail' ),
-(  'John Smith', 'Sales Representative', 'John.Smith@company.mail' )
+    ( 'Peter Johnson', 'CEO', 'Peter.Johnson@company.mail' ),
+    ( 'Jack Richard', 'CTO', 'Jack.Richard@company.mail' ),
+    ( 'Maya Tomas', 'CFO', 'Maya.Tomas@company.mail' ),
+    ( 'Tomas Lindt', 'Lead Software Engineer', 'Tomas.Lindt@company.mail' ),
+    ( 'George Russel', 'Software Engineer', 'George.Russel@company.mail' ),
+    ( 'Anna Rocksmith', 'Junior Software Engineer', 'Anna.Rocksmith@company.mail' ),
+    ( 'Lily Jones', 'Lead Accountant', 'Lily.Jones@company.mail' ),
+    ( 'Lionel Ross', 'Accountant', 'Lionel.Ross@company.mail' ),
+    ( 'Star Light', 'Sales Lead', 'Star.Light@company.mail' ),
+    ( 'John Smith', 'Sales Representative', 'John.Smith@company.mail' )
 
 GO
 
 -- Insert rows into table 'Departments'
 INSERT INTO [Departments]
-( -- Columns to insert data into
- [DepartmentID], [Name], [ManagerID]
-)
+    ( -- Columns to insert data into
+    [DepartmentID], [Name], [ManagerID]
+    )
 VALUES
-(  1, 'Management', 1 ),
-(  2, 'Development', 2 ),
-(  3, 'Finance', 3 ),
-(  4, 'Sales', 9 )
+    ( 1, 'Management', 1 ),
+    ( 2, 'Development', 2 ),
+    ( 3, 'Finance', 3 ),
+    ( 4, 'Sales', 9 )
 GO
+
+-- Add departmentID to Employees
+UPDATE Employees SET [DepartmentID] = 1 WHERE [JobTitle] IN ('CEO', 'CTO', 'CFO')
+UPDATE Employees SET [DepartmentID] = 2 WHERE [JobTitle] IN ('Lead Software Engineer', 'Software Engineer', 'Junior Software Engineer')
+UPDATE Employees SET [DepartmentID] = 3 WHERE [JobTitle] IN ('Lead Accountant', 'Accountant')
+UPDATE Employees SET [DepartmentID] = 4 WHERE [JobTitle] IN ('Sales Lead', 'Sales Representative')
 
 -- Insert rows into table 'Projects'
 INSERT INTO [Projects]
-( -- Columns to insert data into
-[Name], [Description], [Deadline]
-)
+    ( -- Columns to insert data into
+    [Name], [Description], [Deadline]
+    )
 VALUES
-(  'Finance Review', 'Review all the company finances in order to lower company expenditures', '2025-02-28' ), 
-(  'Winter Sale', 'Focus our efforts on maximasing sales for the winter period', '2025-03-30' ), 
-(  'New RocketSoft Release', 'Finish up the development of the next version of our proprietary rocket software', '2025-06-01' )
+    ( 'Finance Review', 'Review all the company finances in order to lower company expenditures', '2025-02-28' ),
+    ( 'Winter Sale', 'Focus our efforts on maximasing sales for the winter period', '2025-03-30' ),
+    ( 'New RocketSoft Release', 'Finish up the development of the next version of our proprietary rocket software', '2025-06-01' )
 
 GO
 
 -- Insert rows into table ''
 INSERT INTO [ProjectEmployees]
-( [ProjectID], [EmployeeID])
+    ( [ProjectID], [EmployeeID])
 VALUES
-( 3, 1), 
-( 3, 3), 
-( 3, 7), 
-( 3, 8), 
-( 4, 1), 
-( 4, 9), 
-( 4, 10), 
-( 5, 2), 
-( 5, 4), 
-( 5, 5), 
-( 5, 6)
+    ( 3, 1),
+    ( 3, 3),
+    ( 3, 7),
+    ( 3, 8),
+    ( 4, 1),
+    ( 4, 9),
+    ( 4, 10),
+    ( 5, 2),
+    ( 5, 4),
+    ( 5, 5),
+    ( 5, 6)
 
 GO
