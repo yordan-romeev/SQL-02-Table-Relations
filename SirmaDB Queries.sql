28a169c91107f26f678c9b447dddcc794b4305e0
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-13 16:58:17 +0200

    Add SirmaDB Queries SQL File

---
+++ SirmaDB Queries.sql
@@ -0,0 +1,34 @@
+--Use `sirmadb.sql` to create the database. Then write the following queries:
+USE SirmaAcademy
+
+--1. **List All Employees**:
+--   Retrieve all employees' full names, job titles, and their salaries.
+
+
+
+--2. **Employees by Department**:
+--   Display the names of employees and their departments.
+
+--3. **List Employees in a Specific Location**:
+--   Retrieve the names and job titles of employees working in the "UK Branch".
+
+--4. **Highest Paid Employee**:
+--  Find the employee with the highest salary.
+
+--5. **Average Salary by Department**:
+--   Calculate the average salary for each department.
+
+--6. **Employees Without a Manager**:
+--   Retrieve the names of employees who are not managers.
+
+--7. **Departments and Their Managers**:
+--   Display each department name along with the full name of its manager.
+
+--8. **Employees in a Specific Country**:
+--   Find all employees working in offices located in "Bulgaria".
+
+--9. **Total Salaries by Country**:
+--   Calculate the total salaries of employees working in each country.
+
+--10. **Employees Earning Above Average Salary**:
+--    Find employees who earn above the average salary for the company.
