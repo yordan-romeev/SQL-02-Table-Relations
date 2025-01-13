785676ba9e8f2e02324acb6b7aae281e115a7c22
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-13 19:16:51 +0200

    Add SirmaDB Queries Tasks 4-6

--- SirmaDB Queries.sql
+++ SirmaDB Queries.sql
@@ -12,24 +12,34 @@ FROM Employees
 
 SELECT e.FirstName, e.LastName, d.DepartmentName
 FROM Employees AS e
-JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
 
 --3. **List Employees in a Specific Location**:
 --   Retrieve the names and job titles of employees working in the "UK Branch".
 
 SELECT e.FirstName, e.LastName, e.JobTitle
 FROM Employees AS e
-JOIN OfficeLocations AS ol ON e.LocationID = ol.LocationID
+    JOIN OfficeLocations AS ol ON e.LocationID = ol.LocationID
 WHERE ol.LocationName = 'UK Branch'
 
 --4. **Highest Paid Employee**:
 --  Find the employee with the highest salary.
+SELECT *
+FROM Employees
+WHERE Salary = (SELECT MAX(Salary) FROM Employees)
 
 --5. **Average Salary by Department**:
 --   Calculate the average salary for each department.
+SELECT d.DepartmentName, AVG(e.Salary) AS [Average Salary]
+FROM Employees AS e
+    JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+GROUP BY d.DepartmentName
 
 --6. **Employees Without a Manager**:
 --   Retrieve the names of employees who are not managers.
+SELECT FirstName, LastName
+FROM Employees
+WHERE EmployeeID NOT IN (SELECT ManagerID FROM Departments)
 
 --7. **Departments and Their Managers**:
 --   Display each department name along with the full name of its manager.
